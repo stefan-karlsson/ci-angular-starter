f74d4ecce1ad5b3e963386ddd70c08f04bb01d9d
Stefan <stefan@successful.se>
2018-11-11 22:32:09 +0100

    feat: initial commit

---
+++ apps/public/web-client/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'public-web-client',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/public/web-client/'
+};
---
+++ apps/public/web-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'slab-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'public-web-client';
+}
